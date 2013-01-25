c57c20598ee3fc70ab75f251baa557307a214dc5
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2013-01-25 12:44:40 +0100

    XMonad: Updating the config

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,149 +1,128 @@
-import XMonad
-import XMonad.Core
+import XMonad ((|||), (.|.), (<+>), (=?), (-->))
+import qualified XMonad as X
 import qualified Data.Map as M
-import XMonad.Actions.CycleWS
-import XMonad.Layout.Spacing
-import XMonad.Layout.PerWorkspace
-import XMonad.Layout.Grid
-import XMonad.Layout.Fullscreen
-import XMonad.ManageHook
+import qualified XMonad.Actions.CycleWS as Cycle
+import qualified XMonad.Layout.Spacing as Space
+import qualified XMonad.Layout.PerWorkspace as WS
+import qualified XMonad.Layout.Grid as Grid
 import qualified XMonad.StackSet as W
-import XMonad.Actions.GridSelect
-import XMonad.Hooks.FadeInactive
-import XMonad.Hooks.UrgencyHook
-import XMonad.Actions.FindEmptyWorkspace
-import XMonad.Prompt
-import XMonad.Prompt.Ssh
-import XMonad.Prompt.Shell
-import XMonad.Prompt.Workspace
-import XMonad.Prompt.Window
-import XMonad.Prompt.AppendFile
-import XMonad.Layout.Spacing
-import XMonad.Actions.PhysicalScreens
-import XMonad.Layout.SimpleFloat
-import XMonad.Layout.ResizableTile
+import qualified XMonad.Actions.GridSelect as GridSelect
+import qualified XMonad.Hooks.FadeInactive as Fade
+import qualified XMonad.Hooks.UrgencyHook as Urgency
+import qualified XMonad.Actions.FindEmptyWorkspace as Empty
+import qualified XMonad.Prompt as Prompt
+import qualified XMonad.Prompt.Ssh as Ssh
+import qualified XMonad.Prompt.Shell as Shell
+import qualified XMonad.Prompt.Window as PW
+import qualified XMonad.Actions.PhysicalScreens as PS
+import qualified XMonad.Layout.ResizableTile as RT
 import qualified XMonad.Prompt.AppLauncher as AL
 
 main = do
-  xmonad $ withUrgencyHook dzenUrgencyHook
-             { args = ["-w", "550", "-fg", "#ffffff"] }
-             $ defaultConfig
-                   { modMask            = mod4Mask
-                   , layoutHook         = myLayout
-                   , workspaces         = ["1",
-                                           "2",
-                                           "3",
-                                           "4",
-                                           "5",
-                                           "msg",
-                                           "sh",
-                                           "mpd",
-                                           "www",
-                                           "irc",
-                                           "ssh",
-                                           "mail",
-                                           "pdf",
-                                           "gfx",
-                                           "dl"
-                                          ]
-                   , manageHook         = newManageHook
-                   , logHook            = myLogHook
-                   , mouseBindings      = newMouse
-                   , keys               = newKeys
-                   , borderWidth        = 2
-                   , terminal           = "valaterm"
-                   , normalBorderColor  = "#1c1c1c"
-                   , focusedBorderColor = "#cd5c5c"
-                   }
+  X.xmonad
+  $ Urgency.withUrgencyHook Urgency.dzenUrgencyHook
+        { Urgency.args = ["-w", "550", "-fg", "#ffffff"] }
+        $ X.defaultConfig
+              { X.modMask            = X.mod4Mask
+              , X.layoutHook         = myLayout
+              , X.workspaces         = myWorkspaces
+              , X.manageHook         = newManageHook
+              , X.logHook            = myLogHook
+              , X.keys               = newKeys
+              , X.borderWidth        = 2
+              , X.terminal           = myTerminal
+              , X.normalBorderColor  = "#1c1c1c"
+              , X.focusedBorderColor = "#cd5c5c"
+              }
 
+myWorkspaces = ["root", "1", "2", "3", "4", "5", "www", "mail", "msg", "irc", "music"]
+myTerminal = "valaterm"
+myFont = "fixed"
+myWorkspaceWindows =
+    [ ("Firefox", "www")
+    , ("Thunderbird", "mail")
+    , ("Pidgin", "msg")
+    ]
+myFloatingWindows = ["Save As...", "Open"]
 
-myXPConfig :: XPConfig
+myXPConfig :: Prompt.XPConfig
 myXPConfig =
-    defaultXPConfig
-    { font        = "fixed"
-    , position    = Top
-    , bgColor     = "#080808"
-    , fgColor     = "#ffffff"
-    , bgHLight    = "#0087ff"
-    , fgHLight    = "#ffffff"
-    , borderColor = "#1c1c1c"
+    Prompt.defaultXPConfig
+    { Prompt.font        = myFont
+    , Prompt.position    = Prompt.Top
+    , Prompt.bgColor     = "#080808"
+    , Prompt.fgColor     = "#ffffff"
+    , Prompt.bgHLight    = "#0087ff"
+    , Prompt.fgHLight    = "#ffffff"
+    , Prompt.borderColor = "#1c1c1c"
     }
 
-myLogHook :: X ()
+myLogHook :: X.X ()
 myLogHook =
-    fadeInactiveLogHook fadeAmount
+    Fade.fadeInactiveLogHook fadeAmount
     where
       fadeAmount = 0.9
 
 myLayout =
-    onWorkspace
-    "www"
-    full_web
-    $ onWorkspace "msg" tiled_msg
-          $ spacing 0 $ Grid ||| tiled
+    WS.onWorkspace
+    "msg"
+    tiled_msg
+    $ Space.spacing 0 $ Grid.Grid ||| tiled
  where
-   tiled = ResizableTall nmaster delta ratio []
+   tiled = RT.ResizableTall nmaster delta ratio []
    nmaster = 1
    delta = 3/100
    ratio = 1/2
-   tiled_msg = Tall nmaster delta ratio_msg
+   tiled_msg = X.Tall nmaster delta ratio_msg
    ratio_msg = 4/5
-   ratio_web = 7/10
-   full_web = ResizableTall nmaster delta ratio_web []
-
-button8 = 8 :: Button
-button9 = 9 :: Button
-
-myMouse x@(XConfig {XMonad.modMask = modm}) =
-    [ -- ((modm,                button5),        (\_ -> sendMessage MirrorShrink))
-      -- ((modm,                button4),        (\w -> windows W.swapDown))
-      ((0,                button8),        (\w -> windows W.shiftMaster))
-    , ((0,                button9),        (\w -> withFocused $ windows . W.sink))
-    ]
-
-newMouse x = M.union (M.fromList (myMouse x)) (mouseBindings defaultConfig x)
 
 newKeys x = (M.fromList (myKeys x))
-myKeys conf@(XConfig {XMonad.modMask = modm}) =
-             [ ((modm,                        xK_c),          kill)
-             , ((modm,                        xK_s),          sshPrompt myXPConfig)
-             , ((modm,                        xK_f),          withFocused $ windows . W.sink)
-             , ((modm .|. controlMask,        xK_Right),      shiftTo Next EmptyWS)
-             , ((modm .|. controlMask,        xK_Left),       shiftTo Prev EmptyWS)
-             , ((modm,                        xK_Right),      moveTo Next NonEmptyWS)
-             , ((modm,                        xK_Left),       moveTo Prev NonEmptyWS)
-             , ((modm,                        xK_k),          windowPromptBring myXPConfig)
-             , ((modm,                        xK_Tab),        windows W.focusDown)
-             , ((modm .|. shiftMask,        xK_Right),        onPrevNeighbour W.shift)
-             , ((modm .|. shiftMask,        xK_Left),         onNextNeighbour W.shift)
-             , ((modm,                        xK_Down),       viewEmptyWorkspace)
-             , ((modm,                        xK_BackSpace),  shellPrompt myXPConfig)
-             , ((0,                        0x1008ff13 ),      spawn "amixer -q sset Master 2dB+")
-             , ((modm,                        xK_z),          spawn "xscreensaver-command -lock")
-             , ((modm,                        xK_space ),     sendMessage NextLayout)
-             , ((0,                        0x1008ff11 ),      spawn "amixer -q sset Master 2dB-")
-             , ((modm,                        xK_Return),     spawn "valaterm")
-             , ((modm,                        xK_Up),         goToSelected $ gsconfig2 myColorizer)
-             , ((modm,                        xK_m),          windows W.shiftMaster)
-             , ((modm,                        xK_w),          sendMessage MirrorShrink)
-             , ((modm,                        xK_x),          sendMessage MirrorExpand)
-             , ((modm,                        xK_h),          sendMessage Shrink)
-             , ((modm,                        xK_l),          sendMessage Expand)
-             ]
-
+myKeys conf@(X.XConfig {X.modMask = modm}) =
+    [ ((modm,                   X.xK_c),         X.kill)
+    , ((modm,                   X.xK_s),         Ssh.sshPrompt myXPConfig)
+    , ((modm,                   X.xK_f),         X.withFocused $ X.windows . W.sink)
+    , ((modm .|. X.controlMask, X.xK_Right),     Cycle.shiftTo Prompt.Next Cycle.EmptyWS)
+    , ((modm .|. X.controlMask, X.xK_Left),      Cycle.shiftTo Prompt.Prev Cycle.EmptyWS)
+    , ((modm,                   X.xK_Right),     Cycle.moveTo Prompt.Next Cycle.NonEmptyWS)
+    , ((modm,                   X.xK_Left),      Cycle.moveTo Prompt.Prev Cycle.NonEmptyWS)
+    , ((modm,                   X.xK_k),         PW.windowPromptBring myXPConfig)
+    , ((modm,                   X.xK_Tab),       X.windows W.focusDown)
+    , ((modm .|. X.shiftMask,   X.xK_Right),     PS.onPrevNeighbour W.shift)
+    , ((modm .|. X.shiftMask,   X.xK_Left),      PS.onNextNeighbour W.shift)
+    , ((modm,                   X.xK_Down),      Empty.viewEmptyWorkspace)
+    , ((modm,                   X.xK_BackSpace), Shell.shellPrompt myXPConfig)
+    , ((0,                      0x1008ff13),     X.spawn "amixer -q sset Master 2dB+")
+    , ((modm,                   X.xK_z),         X.spawn "xscreensaver-command -lock")
+    , ((modm,                   X.xK_space),     X.sendMessage X.NextLayout)
+    , ((0,                      0x1008ff11),     X.spawn "amixer -q sset Master 2dB-")
+    , ((modm,                   X.xK_Return),    X.spawn myTerminal)
+    , ((modm .|. X.shiftMask,   X.xK_Up),        GridSelect.goToSelected $ gsconfig2 myColorizer)
+    , ((modm,                   X.xK_Up),        GridSelect.gridselectWorkspace gsConfig W.greedyView)
+    , ((modm,                   X.xK_m),         X.windows W.shiftMaster)
+    , ((modm,                   X.xK_w),         X.sendMessage RT.MirrorShrink)
+    , ((modm,                   X.xK_x),         X.sendMessage RT.MirrorExpand)
+    , ((modm,                   X.xK_h),         X.sendMessage X.Shrink)
+    , ((modm,                   X.xK_l),         X.sendMessage X.Expand)
+    ]
+   where
+      gsConfig = myBuildGSConfig GridSelect.defaultGSConfig
 
 gsconfig2 colorizer =
-    (buildDefaultGSConfig colorizer)
-    { gs_cellheight = 60,
-      gs_cellwidth = 250,
-      gs_font = "fixed"
+    myBuildGSConfig
+    (GridSelect.buildDefaultGSConfig colorizer)
+
+myBuildGSConfig config =
+    config
+    { GridSelect.gs_cellheight = 60,
+      GridSelect.gs_cellwidth = 250,
+      GridSelect.gs_font = myFont
     }
 
 myColorizer =
-    colorRangeFromClassName
+    GridSelect.colorRangeFromClassName
     (0xD2, 0xF0, 0x81)
-    (0x7D,0xAB,0x00)
-    (0x0D,0x17,0x1A)
+    (0x7D, 0xAB, 0x00)
+    (0x0D, 0x17, 0x1A)
     black
     white
     where
@@ -151,24 +130,11 @@ myColorizer =
       white = maxBound
 
 myManageHook =
-    composeAll
-    [ resource =? "lxappearance"    --> doFloat
-    , className =? "Iceweasel" <&&> resource =? "Dialog" --> doFloat
-    , className =? "Iceweasel" --> doF (W.shift "www")
-    , className =? "Firefox-bin" --> doF (W.shift "www")
-    , className =? "Pidgin" --> doF (W.shift "msg")
-    , className =? "Iceweasel Preferences" --> doFloat
-    , className =? "Save As..." --> doFloat
-    , className =? "Open" --> doFloat
-    , className =? "mplayer2" --> doFloat
-    , resource =? "Downloads" --> doFloat
-    , className =? "Firefox-bin" --> doMaster
-    , title =? "irc" --> doF (W.shift "irc")
-    , title =? "mpd" --> doF (W.shift "mpd")
-    , title =? "ssh" --> doF (W.shift "ssh")
-    , title =? "mail" --> doF (W.shift "mail")
-    ]
-    where
-      doMaster = doF W.shiftMaster
+    X.composeAll
+    $ map (uncurry toWorkspace) myWorkspaceWindows
+          ++ map floatWindow myFloatingWindows
 
-newManageHook = myManageHook <+> manageHook defaultConfig
+newManageHook = myManageHook <+> X.manageHook X.defaultConfig
+
+toWorkspace name workspace = X.className =? name --> X.doF (W.shift workspace)
+floatWindow name = X.className =? name --> X.doFloat
