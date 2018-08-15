58c156189df0a332122033f44f57da4d5f5aee3d
Kate <kit.ty.kate@disroot.org>
2018-08-15 15:53:13 +0100

    Update emacs & vim config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -32,9 +32,9 @@
               tab-width 4
               indent-tabs-mode nil)
 
-;; Whitespace mode (Eighty Column Rule)
+;; Whitespace mode (Eighty Column Rule currently disabled. Add line-tail to enable it)
 (require 'whitespace)
-(setq whitespace-style '(face empty tabs lines-tail trailing))
+(setq whitespace-style '(face empty tabs trailing))
 (global-whitespace-mode t)
 
 ;; Remove whitespaces
@@ -50,15 +50,20 @@
                 (when (stringp method)
                   (member method '("su" "sudo"))))))))
 
+;; Auto-save
+(defconst emacs-auto-save-dir "~/.emacs.d/auto-save/")
+(setq backup-directory-alist `((".*" . , emacs-auto-save-dir)))
+(setq auto-save-file-name-transforms `((".*", emacs-auto-save-dir)))
+
 ;; Packages
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
 ;; OPAM
 (setq opam-share
-  (substring
-    (shell-command-to-string "opam config var share")
-    0 -1))
+      (substring
+       (shell-command-to-string "opam config var share")
+       0 -1))
 
 ;; Extended files
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
@@ -96,6 +101,7 @@
 (require 'merlin)
 (require 'merlin-iedit)
 (add-hook 'tuareg-mode-hook 'merlin-mode t)
+(add-hook 'caml-mode-hook 'merlin-mode t)
 (setq merlin-use-auto-complete-mode 'easy)
 (setq merlin-command 'opam)
 (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
@@ -107,14 +113,33 @@
 (define-key merlin-mode-map (kbd "M-r") 'evil-custom-merlin-iedit)
 
 ;; Patoline
-(require 'patoline-mode)
+;(require 'patoline-mode)
 
 ;; Why3
-(require 'why3)
+;(require 'why3)
 
 ;; Prolog
 (autoload 'prolog-mode "prolog" "Major mode for editing Prolog programs." t)
 (add-to-list 'auto-mode-alist '("\\.pl$" . prolog-mode))
 
+;; Proof General
+(load-file "~/programming/contrib/proofgeneral/generic/proof-site.el")
+(add-hook 'proof-mode-hook
+          '(lambda ()
+             (define-key proof-mode-map [f5] 'proof-assert-next-command-interactive)
+             (define-key proof-mode-map [f6] 'proof-undo-last-successful-command)))
+
 ;; Ott
-(require 'ottmode)
+;(require 'ottmode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (vala-mode markdown-mode iedit evil auto-complete))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
