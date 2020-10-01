39b6e247b8f07f0f5969c4813b7ba4f0722edb69
Kate <kit.ty.kate@disroot.org>
2020-10-01 13:37:49 +0100

    Start firefox in wayland mode & fix link clicks on the terminal

--- startway.sh
+++ startway.sh
@@ -4,5 +4,7 @@
 export GTK2_RC_FILES=$HOME/.gtkrc-2.0
 export GTK_IM_MODULE=xim
 export _JAVA_AWT_WM_NONREPARENTING=1
+export MOZ_ENABLE_WAYLAND=1
+export MOZ_DBUS_REMOTE=1
 
 exec dbus-launch --exit-with-session ssh-agent sway
