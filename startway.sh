6fa3e4a6f6284530a307d50e6ff5cecc5e63d7e3
Kate <kit.ty.kate@disroot.org>
2021-01-07 13:44:55 +0000

    Improve wayland environment

--- startway.sh
+++ startway.sh
@@ -5,8 +5,10 @@ export GTK2_RC_FILES=$HOME/.gtkrc-2.0
 export _JAVA_AWT_WM_NONREPARENTING=1
 export MOZ_ENABLE_WAYLAND=1
 export MOZ_DBUS_REMOTE=1
+export XDG_SESSION_TYPE=wayland
+export SDL_VIDEODRIVER=wayland
 
 # Fix tray icons
 export XDG_CURRENT_DESKTOP=Unity
 
-exec dbus-launch --exit-with-session sway
+exec dbus-launch --exit-with-session /usr/bin/sway
