74f7d625e08c97b09231beaccaff5d41b42bb19d
Kate <kit.ty.kate@disroot.org>
2021-01-15 20:03:01 +0000

    sway: fix the switch focus script in presence of unexpected characters returned by swaymsg -t get_tree

--- .config/sway/focus.sh
+++ .config/sway/focus.sh
@@ -1,22 +1,25 @@
 #!/bin/sh -e
 
-tree=$(swaymsg -t get_tree)
+tree=$(mktemp)
+swaymsg -t get_tree >> "$tree"
 
-current_workspace=$(echo "$tree" | jq '.nodes | .[1].current_workspace')
+current_workspace=$(cat "$tree" | jq '.nodes | .[1].current_workspace')
 
 # Get all the available windows in the workspace
-unfocused_containers=$(echo "$tree" | jq ".nodes | .[1].nodes | .[] | select(if .name == $current_workspace then . else null end) | ..|.nodes? | .[]? | if .pid then .id else null end | select(.)" | sort -g)
+unfocused_containers=$(cat "$tree" | jq ".nodes | .[1].nodes | .[] | select(if .name == $current_workspace then . else null end) | ..|.nodes? | .[]? | if .pid then .id else null end | select(.)" | sort -g)
 
-current_container=$(echo "$tree" | jq ".nodes | .[1].nodes | .[] | select(if .name == $current_workspace then . else null end) | ..|.nodes? | .[]? | if .focused then .id else null end | select(.)")
+current_container=$(cat "$tree" | jq ".nodes | .[1].nodes | .[] | select(if .name == $current_workspace then . else null end) | ..|.nodes? | .[]? | if .focused then .id else null end | select(.)")
 
 next_container=$(echo "$unfocused_containers" | head -n 1)
 for id in $unfocused_containers; do
-  if [ $id -gt $current_container ]; then
+  if test "$id" -gt "$current_container"; then
     next_container=$id
     break
   fi
 done
 
-if [ -n $next_container ]; then
+if test -n $next_container; then
   swaymsg "[con_id=$next_container]" focus
 fi
+
+rm -f "$tree"
